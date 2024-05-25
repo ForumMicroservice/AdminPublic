076b701b5891a53642e5dbf41953f0def5b419c2
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2024-05-25 21:48:32 +0300

    CLearing dashboards

--- src/router/index.ts
+++ src/router/index.ts
@@ -40,33 +40,6 @@ const routes: Array<RouteRecordRaw> = [
         path: 'projects',
         component: () => import('../pages/projects/ProjectsPage.vue'),
       },
-      {
-        name: 'payments',
-        path: '/payments',
-        component: RouteViewComponent,
-        children: [
-          {
-            name: 'payment-methods',
-            path: 'payment-methods',
-            component: () => import('../pages/payments/PaymentsPage.vue'),
-          },
-          {
-            name: 'billing',
-            path: 'billing',
-            component: () => import('../pages/billing/BillingPage.vue'),
-          },
-          {
-            name: 'pricing-plans',
-            path: 'pricing-plans',
-            component: () => import('../pages/pricing-plans/PricingPlans.vue'),
-          },
-        ],
-      },
-      {
-        name: 'faq',
-        path: '/faq',
-        component: () => import('../pages/faq/FaqPage.vue'),
-      },
     ],
   },
   {
